06c7bf03fc655aa486ff3ad8cc25dd815514ef91
Stephanie <stephcshea@gmail.com>
2018-06-01 19:03:28 -0400

    working on search using xpath

--- BotReddit/Program.cs
+++ BotReddit/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace BotReddit
 {
@@ -6,7 +10,19 @@ namespace BotReddit
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var options = new ChromeOptions();
+            options.AddArguments("--disable-gpu");
+
+            var chromeDriver = new ChromeDriver();
+            chromeDriver.Navigate().GoToUrl("http://reddit.com");
+            //chromeDriver.FindElementByXPath("//input[@id='login']").Click();
+            chromeDriver.FindElementByXPath("//input[@id='search']").Click();
+            //chromeDriver.FindElementByXPath("//*[@id_\"search\"]/input[1]").Click();
+            chromeDriver.Keyboard.SendKeys("test");
+            chromeDriver.Keyboard.SendKeys(Keys.Enter);
+
+                Console.WriteLine("Hello Reddit!");
+
         }
     }
 }
